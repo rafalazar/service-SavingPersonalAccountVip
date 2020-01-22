87c15bd45aa567bab97a9f56b2b0d53ba1cfe9d2
Jordan <jcr.salazar@outlook.com>
2020-01-22 14:47:01 -0500

    Microservicio creado satisfactoriamente.

---
+++ src/main/java/com/rafalazar/bootcamp/app/ServiceSavingPersonalAccountVipApplication.java
@@ -0,0 +1,13 @@
+package com.rafalazar.bootcamp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSavingPersonalAccountVipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSavingPersonalAccountVipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafalazar/bootcamp/app/ServiceSavingPersonalAccountVipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafalazar.bootcamp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSavingPersonalAccountVipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
